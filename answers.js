705aedc8149c8b13854648955f5af55abdbb1397
Aliyah Jessa <aliyah@aliyahmaliyah.com>
2016-01-27 18:28:31 +0000

    Function that takes a string and returns last character.

--- answers.js
+++ answers.js
@@ -5,3 +5,13 @@ function FirstLetter(string){
     return (string.charAt(0));
 }
 FirstLetter("I like coffee.");
+FirstLetter("");
+FirstLetter("Linguini likes avocados!");
+
+//Write a function that takes a string and returns the last character of a string.
+function lastLetter(string) {
+    return (string.charAt(string.length - 1));
+}
+lastLetter("I like coffee.");
+lastLetter("");
+lastLetter("Linguini likes avocados!");
