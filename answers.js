21385b70597159c6c9463e6be2d3103ed51e9214
Aliyah Jessa <aliyah@aliyahmaliyah.com>
2016-01-27 18:21:18 +0000

    Function that takes a string and returns the 1st character of that string

---
+++ answers.js
@@ -0,0 +1,7 @@
+//These are my answers for the functions excercises 27/01/16
+
+//Write a function that takes a string and returns the first character of the string.
+function FirstLetter(string){
+    return (string.charAt(0));
+}
+FirstLetter("I like coffee.");
